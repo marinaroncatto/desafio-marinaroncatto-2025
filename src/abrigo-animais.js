a2a5e12c79011681aa9bf33246d668766b3614f7
marinaroncatto <marinam.roncatto@gmail.com>
2025-09-04 20:19:58 -0300

    Mapeamento dos animais e filtro de entradas na função

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,6 +1,22 @@
 class AbrigoAnimais {
 
+  static animaisDoAbrigo ={
+    REX: { tipo: 'CAO', brinquedos: ['RATO', 'BOLA'] },
+    MIMI: { tipo: 'GATO', brinquedos: ['BOLA', 'LASER'] },
+    FOFO: { tipo: 'GATO', brinquedos: ['BOLA', 'RATO', 'LASER'] },
+    ZERO: { tipo: 'GATO', brinquedos: ['RATO', 'BOLA'] },
+    BOLA: { tipo: 'CAO', brinquedos: ['CAIXA', 'NOVELO'] },
+    BEBE: { tipo: 'CAO', brinquedos: ['LASER', 'RATO', 'BOLA'] },
+    LOCO: { tipo: 'JABUTI', brinquedos: ['SKATE', 'RATO'] }  
+    
+  }
+
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+
+    const pessoa1 = brinquedosPessoa1.split(',').map(x => x.trim().toUpperCase());
+    const pessoa2 = brinquedosPessoa2.split(',').map(x => x.trim().toUpperCase());
+    const ordemBichinhos = ordemAnimais.split(',').map(x => x.trim().toUpperCase());    
+
   }
 }
 
